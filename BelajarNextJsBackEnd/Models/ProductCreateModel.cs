af33f0bf274b4cf6ee7853f4e54c465267963f4d
OtakKentung <95601654+vannessiwata@users.noreply.github.com>
2023-04-13 12:49:25 +0700

    Back-End for Product and Brand

---
+++ BelajarNextJsBackEnd/Models/ProductCreateModel.cs
@@ -0,0 +1,22 @@
+﻿namespace BelajarNextJsBackEnd.Models
+{
+    public class ProductCreateModel
+    {
+        public string Name { get; set; } = "";
+        public string Description { get; set; } = "";
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public string BrandId { get; set; } = "";
+    }
+
+    public class ProductDetailModel
+    {
+        public string Id { get; set; } = "";
+        public string Name { get; set; } = "";
+        public string Description { get; set; } = "";
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+        public string BrandId { get; set; } = "";
+        public string BrandName { get; set; } = "";
+    }
+}
