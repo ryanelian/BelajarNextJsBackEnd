26b935efaf8defc4d08a2c01795be3b5d8526eb7
jovansteven <115369731+jovansteven@users.noreply.github.com>
2023-04-13 15:26:47 +0700

    Add files via upload

---
+++ BelajarNextJsBackEnd/Models/ProductCreateModel.cs
@@ -0,0 +1,32 @@
+﻿namespace BelajarNextJsBackEnd.Models
+{
+    public class ProductCreateModel
+    {
+        public string Name { get; set; } = "";
+
+        public string Description { get; set; } = "";
+
+        public decimal Price { get; set; }
+
+        public int Quantity { set; get; }
+
+        public string BrandId { get; set; } = "";
+    }
+
+    public class ProductDetailModel
+    {
+        public string Id { set; get; } = "";
+
+        public string Name { set; get; } = "";
+
+        public string Description { get; set; } = "";
+
+        public decimal Price { get; set; }
+
+        public int Quantity { set; get; }
+
+        public string BraindId { set; get; } = "";
+
+        public string BrandName { set; get; } = "";
+    }
+}
