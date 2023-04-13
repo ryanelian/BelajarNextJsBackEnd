4573fa08759710fbc82dca7107af56efc23d1464
JoSach07 <jo.sachio07@gmail.com>
2023-04-13 13:16:10 +0700

    Added Products and Brands controller and models

---
+++ BelajarNextJsBackEnd/Models/ProductUpdateModel.cs
@@ -0,0 +1,15 @@
+﻿namespace BelajarNextJsBackEnd.Models
+{
+    public class ProductUpdateModel
+    {
+        public string Name { get; set; } = "";
+
+        public string Description { get; set; } = "";
+
+        public decimal Price { get; set; }
+
+        public int Quantity { set; get; }
+
+        public string BrandId { set; get; } = "";
+    }
+}
