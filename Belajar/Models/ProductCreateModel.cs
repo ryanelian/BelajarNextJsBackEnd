a70bfb0b48a0cc757eb9b258cb244b972d86fbd9
crislim21 <reavleaz@yahoo.co.id>
2023-04-14 10:29:07 +0700

    update brandname for productdetailmodel

--- Belajar/Models/ProductCreateModel.cs
+++ Belajar/Models/ProductCreateModel.cs
@@ -9,14 +9,4 @@
         public string Description { get; set; } = "";
 
     }
-    public class ProductDetailModel
-    {
-        public string Id { get; set; } = "";
-        public string Name { get; set; } = "";
-        public string BrandId { get; set; } = "";
-        public string BrandName { get; set; } = "";
-        public decimal Price { get; set; }
-        public int Quantity { get; set; } 
-        public string Description { get; set; } = "";
-    }
 }
---
+++ Belajar/Models/ProductDetailModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Belajar.Models
+{
+    public class ProductDetailModel
+    {
+        public string Id { get; set; } = "";
+        public string Name { get; set; } = "";
+        public string BrandId { get; set; } = "";
+        public string BrandName { get; set; } = "";
+        public decimal Price { get; set; }
+        public int Quantity { get; set; } 
+        public string Description { get; set; } = "";
+    }
+}
